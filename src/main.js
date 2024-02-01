55d1b627c25ffd390225ce8fd1fb6602c520d4cd
Irina-Piskokha <4526937@gmail.com>
2024-02-01 15:21:29 +0200

    third day

--- src/main.js
+++ src/main.js
@@ -9,9 +9,10 @@ import { serchPictureGallery } from './services/api';
 import { buttonLoadMore } from './services/buttonLoadMore';
 
 const pageSizeParams = {
+  query: '',
   page: 1,
   maxPage: 0,
-  pageSize: 40,
+  per_page: 40,
 };
 
 refs.loader.style.display = 'none';
@@ -22,16 +23,31 @@ async function onFormSubmit(event) {
   event.preventDefault();
 
   refs.ulContainer.innerHTML = '';
+  pageSizeParams.page = 1;
   refs.loader.style.display = 'block';
   const form = event.currentTarget;
-  const input = form.elements.text.value.trim();
+  pageSizeParams.query = form.elements.text.value.trim();
+
+  if (!pageSizeParams.query) {
+    refs.loader.style.display = 'none';
+    refs.buttonLoadMore.removeEventListener('click', onButtonLoadMoreClick);
+    buttonLoadMore.hide(refs.buttonLoadMore);
+    iziToast.error({
+      message: 'Sorry, you have not entered anything!',
+      position: 'topRight',
+      backgroundColor: '#EF4040',
+      color: '#FAFAFB',
+    });
+    return;
+  }
 
   try {
-    const { hits } = await serchPictureGallery(input);
-    console.log(hits);
+    const { hits, totalHits } = await serchPictureGallery(pageSizeParams);
+    pageSizeParams.maxPage = Math.ceil(totalHits / pageSizeParams.per_page);
     refs.loader.style.display = 'none';
 
     if (hits.length === 0) {
+      buttonLoadMore.hide(refs.buttonLoadMore);
       iziToast.error({
         message:
           'Sorry, there are no images matching your search query. Please try again!',
@@ -41,17 +57,7 @@ async function onFormSubmit(event) {
       });
     }
 
-    if (!input) {
-      iziToast.error({
-        message: 'Sorry, you have not entered anything!',
-        position: 'topRight',
-        backgroundColor: '#EF4040',
-        color: '#FAFAFB',
-      });
-      return;
-    }
-
-    refs.ulContainer.innerHTML = markupGallery(hits);
+    refs.ulContainer.innerHTML = markupGallery(hits); // розмітка
 
     const lightbox = new SimpleLightbox('.list_gallery a', {
       caption: true,
@@ -61,7 +67,7 @@ async function onFormSubmit(event) {
     });
     lightbox.refresh();
 
-    if (hits.length > 0 && hits.length !== totalResults) {
+    if (hits.length > 0 && hits.length !== totalHits) {
       buttonLoadMore.show(refs.buttonLoadMore);
       refs.buttonLoadMore.addEventListener('click', onButtonLoadMoreClick);
     } else {
@@ -70,12 +76,41 @@ async function onFormSubmit(event) {
   } catch (error) {
     console.log(error);
     refs.loader.style.display = 'none';
-  } finally {
-    form.reset();
   }
 }
 
 async function onButtonLoadMoreClick() {
   pageSizeParams.page += 1;
   buttonLoadMore.disable(buttonLoadMore);
+
+  try {
+    const { hits } = await serchPictureGallery(pageSizeParams);
+
+    const markup = markupGallery(hits);
+    refs.ulContainer.insertAdjacentHTML('beforeend', markup); // розмітка
+
+    const rect = refs.ulContainer.getBoundingClientRect();
+    console.log(rect);
+    window.scrollBy({
+      top: 800,
+      left: 50,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    buttonLoadMore.enable(buttonLoadMore);
+
+    if (pageSizeParams.page === pageSizeParams.maxPage) {
+      buttonLoadMore.hide(buttonLoadMore);
+      iziToast.error({
+        message:
+          'We are sorry, but you have reached the end of search results.',
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        color: '#FAFAFB',
+      });
+      refs.buttonLoadMore.removeEventListener('click', onButtonLoadMoreClick);
+    }
+  }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -3,15 +3,17 @@ import apiInstance from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '41980985-8125375fe1d9f9e65ca18808e';
 
-function serchPictureGallery(picture) {
+function serchPictureGallery({ query, page = 1, per_page }) {
   return apiInstance
     .get(`${BASE_URL}`, {
       params: {
         key: API_KEY,
-        q: picture,
+        q: query,
         image_type: 'photo',
         image_type: 'horizontal',
         safesearch: true,
+        per_page,
+        page,
       },
     })
     .then(({ data }) => data);
